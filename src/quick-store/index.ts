57a45e7e08e1cb4a6373ce4459a041e816bcad5f
zhen <kelly811124811124@hotmail.com>
2020-02-12 15:33:41 +0800

    create file

--- src/quick-store/index.ts
+++ src/quick-store/index.ts
@@ -6,7 +6,9 @@ import { Schema } from './schema';
 // per file.
 export function quickStore(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    console.log(_options.name)
+    const AKITA_PATH = './src/app/@akita/';
+    const storeName = _options.name;
+    tree.create(`${AKITA_PATH}/mock/${storeName}-mock.service.ts`, `console.log('hello ${_options.name}')`)
     return tree;
   };
 }
