1499bfe0424a9383644ca221d5b1061ba4ff9f68
zhen <kelly811124811124@hotmail.com>
2020-02-12 15:02:32 +0800

    add schema option type

--- src/quick-store/index.ts
+++ src/quick-store/index.ts
@@ -1,10 +1,12 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Schema } from './schema';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function quickStore(_options: any): Rule {
+export function quickStore(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    console.log(_options.name)
     return tree;
   };
 }
