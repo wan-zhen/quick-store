bad10618bc3f8f7b11c7a7b3062ab64c15845167
zhen <kelly811124811124@hotmail.com>
2020-02-17 11:47:38 +0800

    insert module code

--- src/quick-store/index.ts
+++ src/quick-store/index.ts
@@ -1,29 +1,64 @@
 import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
 import { Schema } from './schema';
 import { strings } from '@angular-devkit/core';
-//import * as ts from 'typescript';
-
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function quickStore(_options: Schema): Rule {
   return async (tree: Tree, _context: SchematicContext) => {
     const AKITA_PATH = './src/app/@akita';
     const storeName = _options.name;
-
+    // add index code
     ['mock', 'query', 'services', 'store']
       .forEach(file => {
         if (file === 'mock' && !_options.hasMock) {
           return
         }
+        const path = `${AKITA_PATH}/${file}/index.ts`;
         tree.commitUpdate(
-          tree.beginUpdate(`${AKITA_PATH}/${file}/index.ts`)
+          tree.beginUpdate(path)
             .insertLeft(
-              (tree.read(`${AKITA_PATH}/${file}/index.ts`) || []).toString().length,
+              (tree.read(path) || []).toString().length,
               `export * from './${strings.dasherize(storeName)}.${file}';\n`
             )
         )
-      })
+      });
 
+    // edit module code
+    const modulePath = `${AKITA_PATH}/@akita.module.ts`;
+    const importModuleName = ['MockService', 'Query', 'Service', 'Store'];
+    ['mock', 'queries', 'services', 'stores']
+      .forEach((file, index) => {
+        if (file === 'mock' && !_options.hasMock) {
+          return
+        }
+        tree.commitUpdate(
+          tree.beginUpdate(modulePath)
+            .insertLeft(
+              (tree.read(modulePath) || []).toString().indexOf(`} from './${file}'`),
+              `  ${strings.classify(storeName)}${importModuleName[index]},\n`
+            )
+        )
+      });
+    // insert provider
+    tree.commitUpdate(
+      tree.beginUpdate(modulePath)
+        .insertLeft(
+          (tree.read(modulePath) || []).toString().indexOf(`{ provide:`),
+          `  ${strings.classify(storeName)}Query,\n  ${strings.classify(storeName)}Store,\n`
+        )
+    )
+
+    tree.commitUpdate(
+      tree.beginUpdate(modulePath)
+        .insertLeft(
+          (tree.read(modulePath) || []).toString().indexOf(`];`),
+          _options.hasMock ?
+            `  { provide: ${strings.classify(storeName)}Service, useClass: useMock ? ${strings.classify(storeName)}MockService : ${strings.classify(storeName)}Service },\n`
+            :
+            `  ${strings.classify(storeName)}Service,\n`
+        )
+    )
+    //add template
     const sourceTemplates = url('./files'); // 使用範本
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
